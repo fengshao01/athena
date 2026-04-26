5261236462f1b00da72d4c4a6860ebfdaa95283b
fengshao <fshao01@gmail.com>
2026-04-26 11:20:45 -0700

    set homepage to notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Footer from "@/components/Footer";
-import Greeting from "@/components/Greeting";
-import RemembersList from "@/components/RemembersList";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex flex-1 flex-col bg-zinc-50 dark:bg-black">
-      <main className="mx-auto flex w-full max-w-2xl flex-1 flex-col gap-10 px-6 py-16 sm:py-24">
-        <Greeting />
-        <RemembersList />
-      </main>
-      <Footer />
-    </div>
-  );
+  redirect("/notes");
 }
